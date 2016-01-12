fbfba8b82ef7522687d8586d2b48d14583ed3478
Marcin Jackowski <marcin.jackowski@droidsonroids.pl>
2016-01-12 20:57:08 +0100

    Initial commit

---
+++ OSXNavigation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OSXNavigation
+//
+//  Created by Marcin Jackowski on 12/01/16.
+//  Copyright © 2016 Marcin Jackowski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
