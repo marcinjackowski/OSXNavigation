6afab63b8fe19ab9276d3d90af0fd5c6a6dc6e88
Marcin Jackowski <marcin.jackowski@droidsonroids.pl>
2016-01-12 22:33:34 +0100

    Add asset, fix pop controller

--- OSXNavigation/ViewController.swift
+++ OSXNavigation/ViewController.swift
@@ -9,18 +9,38 @@
 import Foundation
 import Cocoa
 
+enum Operation {
+    case Push, Pop
+    
+    var values: (first: Int, second: Int) {
+        switch self {
+        case .Push:
+            return (0,1)
+        case .Pop:
+            return (1,0)
+        }
+    }
+}
+
 class ViewController: NSViewController {
     func popViewController(viewController: ViewController, currentViewController: ViewController) {
+        setSegmentedControl(.Pop, currentViewController: nil)
         viewController.dismissViewController(currentViewController)
     }
     
     func pushViewController(viewController: ViewController) {
+        let animator = CustomPresentationAnimator()
+        setSegmentedControl(.Push, currentViewController: viewController)
+        presentViewController(viewController, animator: animator)
+    }
+    
+    private func setSegmentedControl(operation: Operation, currentViewController: ViewController?) {
         guard let window = NSApplication.sharedApplication().mainWindow?.windowController as? WindowViewController else { return }
         
-        window.currentViewController = viewController
-        window.segmentedControl.setEnabled(true, forSegment: 0)
-        window.segmentedControl.setEnabled(false, forSegment: 1)
-        let animator = CustomPresentationAnimator()
-        presentViewController(viewController, animator: animator)
+        if let currentViewController = currentViewController {
+            window.currentViewController = currentViewController
+        }
+        window.segmentedControl.setEnabled(true, forSegment: operation.values.first)
+        window.segmentedControl.setEnabled(false, forSegment: operation.values.second)
     }
 }
